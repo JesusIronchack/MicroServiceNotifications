d020cf7e2e5db3aa16a2d7ce3ce4bf29555e9956
JesusCiber <fjesuscl@gmail.com>
2025-05-22 17:27:05 +0200

    init commit

---
+++ src/main/java/com/example/MicroServiceNotifications/MicroServiceNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroServiceNotifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServiceNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServiceNotificationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MicroServiceNotifications/MicroServiceNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroServiceNotifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
